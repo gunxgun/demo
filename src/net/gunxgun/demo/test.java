d84320eef261b942dbf67cbc75712b27bb0d9234
hsupc <hsupc@4-2009-88.iconbio.com.tw>
2012-09-19 17:00:20 +0800

    init

---
+++ src/net/gunxgun/demo/test.java
@@ -0,0 +1,14 @@
+package net.gunxgun.demo;
+
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+
+	}
+
+}
